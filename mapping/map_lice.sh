28014272f9406714bb36065b425807ef42ac2533
mcstitzer <mcstitzer@ucdavis.edu>
2017-09-22 10:33:27 -0700

    Create map_lice.sh

---
+++ mapping/map_lice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -login
+#SBATCH -D /group/jrigrp8/mstitzer/lice/mapping
+#SBATCH -o /group/jrigrp8/mstitzer/lice/slurm-log/mapteo-stdout-%j.txt
+#SBATCH -e /group/jrigrp8/mstitzer/lice/slurm-log/mapteo-stderr-%j.txt
+#SBATCH -J mapteo
+set -e
+set -u
+
+
+CPU=12
+
+## get the sample we want to run for this array iteration
+mapfile -t SAMPLENAMES < lice.samplenames.txt  ## this is a file with each lice identifier on a separate line
+SAMPLENAME=${SAMPLENAMES[$SLURM_ARRAY_TASK_ID]}
+echo ${SAMPLENAME}
+
+READ1=${SAMPLENAME}_1.fastq
+READ2=${SAMPLENAME}_2.fastq
+
+## map all and output sorted bam
+module load bwa
+bwa mem -a -t $CPU $READ1 $READ2 | samtools view -Su  - | samtools sort - ${SAMPLENAME}.sorted
+
