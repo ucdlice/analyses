0a3d3c2630f41241cfb608fc28cee04b15931346
mcstitzer <mcstitzer@ucdavis.edu>
2017-09-30 11:24:45 -0700

    Update gatk_each_individ.sh

--- call_snps/gatk_each_individ.sh
+++ call_snps/gatk_each_individ.sh
@@ -17,13 +17,13 @@ mkdir -p base_recalibration
 for i in `seq 1 96`;
 do
 ## mark duplicates
-if [ ! -f deduped_reads/lice${i}.dedup.bam ]
+if [ ! -f deduped_reads/lice${i}.dedup.bam ];
 then
 java -jar /share/apps/picard-tools-2.7.1/picard.jar MarkDuplicates INPUT=../mapping/lice${i}.sorted.bam OUTPUT=deduped_reads/lice${i}.dedup.bam METRICS_FILE=deduped_reads/lice${i}.dedup.metrics.txt
 fi
 
 ## recalibrate base quality
-if [ ! -f base_recalibration/lice${i}.bqsr.bam ]
+if [ ! -f base_recalibration/lice${i}.bqsr.bam ];
 then
 java -jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar \
    -T PrintReads \
@@ -34,7 +34,7 @@ java -jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar \
 fi
 
 ## call variants in this individual, outputting gvcf
-if [ ! -f lice${i}.gvcf ]
+if [ ! -f lice${i}.gvcf ];
 then
 java ‐jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar \
    -T HaplotypeCaller \
