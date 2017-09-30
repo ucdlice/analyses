0bcb03e14d5f1b2b3eb69a2bc94a64b1602d0fed
mcstitzer <mcstitzer@ucdavis.edu>
2017-09-30 10:59:55 -0700

    Update and rename gatk.sh to gatk_each_individ.sh

---
+++ call_snps/gatk_each_individ.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -login
+#SBATCH -D /group/jrigrp8/mstitzer/lice/call_snps
+#SBATCH -o /group/jrigrp8/mstitzer/lice/slurm-log/genotype-stdout-%j.txt
+#SBATCH -e /group/jrigrp8/mstitzer/lice/slurm-log/genotype-stderr-%j.txt
+#SBATCH -J genotype
+set -e
+set -u
+
+module load R/3.3.1
+module load maven
+module load java
+module load GATK
+
+mkdir -p deduped_reads
+mkdir -p base_recalibration
+
+for i in `seq 1 96`;
+do
+## mark duplicates
+java -jar /share/apps/picard-tools-2.7.1/picard.jar MarkDuplicates INPUT=../mapping/lice${i}.sorted.bam OUTPUT=deduped_reads/lice${i}.dedup.bam METRICS_FILE=deduped_reads/lice${i}.dedup.metrics.txt
+## recalibrate base quality
+ 
+java -jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar \
+   -T PrintReads \
+   -R ../mapping/PhumU2.Riesia.fa \
+   -I deduped_reads/lice${i}.dedup.bam \
+   -BQSR base_recalibration/lice${i}.recalibration_report.grp \
+   -o base_recalibration/lice${i}.bqsr.bam
+
+## call variants in this individual, outputting gvcf
+java ‐jar /share/apps/GATK-3.6/GenomeAnalysisTK.jar \
+   -T HaplotypeCaller \
+   -R ../mapping/PhumU2.Riesia.fa \
+   -I base_recalibration/lice${i}.bqsr.bam \
+   -o lice${i}.gvcf \
+   -ERC GVCF \
+
+done
+
+
+## gvcf info here https://software.broadinstitute.org/gatk/documentation/article?id=3893
+## and here https://qcb.ucla.edu/wp-content/uploads/sites/14/2016/03/GATK_Discovery_Tutorial-Worksheet-AUS2016.pdf
+
+## genotype together 
+#GenotypeGVCFs
+
+# java‐jarGenomeAnalysisTK.jar\
+#‐TGenotypeGVCFs\
+#‐Rref/human_g1k_b37_20.fasta\
+#‐Vsandbox/NA12878_wgs_20.g.vcf\
+#‐Vgvcfs/NA12877_wgs_20.g.vcf\
+#‐Vgvcfs/NA12882_wgs_20.g.vcf\
+#‐osandbox/CEUTrio_wgs_20_GGVCFs_jointcalls.vcf\
+#‐L20:10000000‐10200000
+
+## 4. Variant recalibration
+#Finally, resume the classic GATK Best Practices workflow by running VQSR on these "regular" VCFs according to our usual recommendations.
+
+
+## don't need to merge
+
+### joint genotype
+
+ java -jar GenomeAnalysisTK.jar \
+   -T GenotypeGVCFs \
+   -R reference.fasta \
+   --variant sample1.g.vcf \
+   --variant sample2.g.vcf \
+   -o output.vcf
+   
+   
+## lots of recalibration options
