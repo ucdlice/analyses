4611d57e4ee414eb75e5669e83efd6515ded4e43
pbrec <pbrand@ucdavis.edu>
2017-11-20 20:39:01 -0800

    snprelate pca mt symbiont

--- pca/pca.R
+++ pca/pca.R
@@ -1,22 +1,31 @@
 setwd('/Users/pbrand/science/research/ped/analyses/pca/')
 library(SNPRelate)
-bed<-"all_lice_geno0.5.bed"
-fam<-"all_lice_geno0.5.fam"
-bim<-"all_lice_geno0.5.bim"
+bed<-"lice_only_2.bed"
+fam<-"lice_only_2.fam"
+bim<-"lice_only_2.bim"
 
-snpgdsBED2GDS(bed,fam,bim,"all_lice_geno0.5.gds")
-snpgdsSummary("all_lice_geno0.5.gds")
+snpgdsBED2GDS(bed,fam,bim,"lice_only_2.gds")
+snpgdsSummary("lice_only_2.gds")
 
-genofile<-snpgdsOpen("all_lice_geno0.5.gds")
+genofile<-snpgdsOpen("NC_pre_2.gds",allow.duplicate = T)
+
+
 
 genofile
 
 sample.id<-read.gdsn(index.gdsn(genofile, "sample.id"))
 
+
+
 #use this to subsample data before PCA
-popraw<-read.table("popid.txt")
+qc<-read.table("qc_ind.txt")
+popraw<-read.table("analyses/pca/popid.txt")
+
+sel<-popraw[popraw[,1] %in% qc[,1],1]
+
 sel<-as.vector(popraw[popraw[,3]=="SF",1])
 sel<-popraw[,1]
+
 #pca
 pca<-snpgdsPCA(genofile,autosome.only = F,sample.id = sel)
 pc.percent<-pca$varprop*100
@@ -36,9 +45,9 @@ plot(pca$eigenvect[,c(2,1)], xlab="eigenvector 2", ylab="eigenvector 1")
 
 #plot with info
 sample.id<-read.gdsn(index.gdsn(genofile, "sample.id"))
-popraw<-read.table("popid.txt")
+popraw<-read.table("analyses/pca/popid.txt")
 popraw
-pop_code<-popraw[,3]
+pop_code<-popraw[,2]
 cbind(sample.id,pop_code)
 
 tab <- data.frame(sample.id = pca$sample.id,
@@ -50,10 +59,10 @@ tab <- data.frame(sample.id = pca$sample.id,
                   stringsAsFactors = FALSE)
 head(tab)
 
-plot(tab$EV2, tab$EV1, col=as.integer(tab$pop), xlab="eigenvector 2", ylab="eigenvector 1")
+plot(tab$EV3, tab$EV2, col=as.integer(tab$pop), xlab="eigenvector 2", ylab="eigenvector 1")
 legend("bottomright", legend=levels(tab$pop), pch="o", col=1:nlevels(tab$pop))
 
-text(tab$EV2,tab$EV1,sample.id,adj = 2,cex=0.5)
+text(tab$EV3,tab$EV2,sample.id,adj = 1,cex=0.5)
 
 
 lbls <- paste("PC", 1:4, "\n", format(pc.percent[1:4], digits=2), "%", sep="")
