4d74e8469ff719813762980b806525301979ae19
Michelle Stitzer <mcstitzer@ucdavis.edu>
2017-11-20 19:48:36 -0800

    run admixture

--- admixture/run_admixture.sh
+++ admixture/run_admixture.sh
@@ -5,9 +5,9 @@
 #plink --file ../partition_h2/TE_pedfile --geno 0.1 --make-bed --out TE_cleaned
 
 
-plink --vcf ../pca/all_lice.raw.nodel.vcf.gz --geno 0.1 --make-bed --allow-extra-chr --out all_lice_cleaned
+#plink --vcf ../pca/all_lice.raw.nodel.vcf.gz --geno 0.1 --make-bed --allow-extra-chr --out all_lice_cleaned
 
-plinkbed=all_lice_cleaned.bed
+plinkbed=all_lice_filtered_snps.ldprune.bed
 
 for K in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15;\
 #for K in 11 12 13 14 15;\
